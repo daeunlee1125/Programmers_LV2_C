387c233558e5d8261525bb2cb5ac867c4ee0a781
unknown <bragon@cocoquiet.com>
2026-01-22 16:36:00 +0900

    26.01.22

--- 방문 길이.c
+++ 방문 길이.c
@@ -23,26 +23,57 @@ dirs는 string형으로 주어지며, 'U', 'D', 'R', 'L' 이외에 문자는 주
 dirs의 길이는 500 이하의 자연수입니다.
 */
 
-// 2026.01.17 12:38
-int cmp(const void* a, const void* b) {
-    int aa = *(const int*)a; int bb = *(const int*)b;
-    if (aa>bb) return 1;
-    if (aa==bb) return 0;
-    if (aa<bb) return -1;
-}
-int ncmp(const void* a, const void* b) {
-    int aa = *(const int*)a; int bb = *(const int*)b;
-    if (aa<bb) return 1;
-    if (aa==bb) return 0;
-    if (aa>bb) return -1;
-}
+// 26.01.22 16:11
+typedef struct {
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+}Edge;
 
-int solution(int A[], size_t A_len, int B[], size_t B_len) {
+int solution(const char* dirs) {
     int answer = 0;
+    Edge visited[300];
+    int now[2] = {0, 0};
+    int vcnt = 0;
     
-    qsort(A, A_len, sizeof(int), cmp);
-    qsort(B, B_len, sizeof(int), ncmp);
-    for (int i = 0; i < A_len; i++) answer += (A[i] * B[i]);
+    for (int i = 0; i < strlen(dirs); i++) {
+        int newX = now[0];
+        int newY = now[1];
+        int plusf = 0;
+        switch (dirs[i]) {
+            case 'U': newY++; plusf++; break;
+            case 'D': newY--; break;
+            case 'R': newX++; plusf++; break;
+            case 'L': newX--; break;
+        }
+        if (newX>5 || newX<-5 || newY>5 || newY<-5) continue;
+        
+        int flag = 0;
+        int sx, sy, bx, by;
+        if (plusf == 1) {
+            sx = now[0]; sy = now[1]; bx = newX; by = newY;
+        } else {
+            sx = newX; sy = newY; bx = now[0]; by = now[1];
+        }
+        
+        for (int i = 0; i < vcnt; i++) {
+            if (visited[i].x1==sx && visited[i].y1==sy && visited[i].x2==bx && visited[i].y2==by) {
+                flag = 1;
+                break;
+            }
+        }
+        
+        if (flag==0) {
+            answer++;
+            visited[vcnt].x1 = sx;
+            visited[vcnt].y1 = sy;
+            visited[vcnt].x2 = bx;
+            visited[vcnt++].y2 = by;
+        }
+        now[0] = newX;
+        now[1] = newY;
+    }
     
     return answer;
 }
